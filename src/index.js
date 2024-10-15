7535efadc80e441174f1446cdcda48cb325f59be
Emmanuel Sánchez <emmsanchezaranda@hotmail.com>
2024-10-14 23:49:16 -0600

    CORS problem fixed allowing origins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import express from "express";
+import cors from "cors";
 import { QuestionController } from "./controllers/questions.js";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://quiz-app-web-plum.vercel.app/",
+];
 
 const app = express();
 app.disable("x-powered-by");
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 const PORT = process.env.PORT ?? 1234;
 
